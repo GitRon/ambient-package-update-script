caa8b8455bd684a04a588ab147b7415be95bd382
Ronny Vedrilla <ronny.vedrilla@ambient.digital>
2025-02-15 09:31:35 +0100

    Improvements

--- update_projects.py
+++ update_projects.py
@@ -40,7 +40,16 @@ class PackageUpdater:
             else:
                 self._print_red(f"> {result.stdout}")
 
-    def _increment_version(self, file_path: str):
+    def _create_header(self, package_name: str):
+        title = f"# Processing {package_name} #"
+        max_length = len(title)
+
+        # Erstelle die Ausgabe mit der richtigen Länge
+        decorative_line = "#" * max_length
+
+        self._print_cyan(f"{decorative_line}\n{title}\n{decorative_line}")
+
+    def _get_next_version(self, file_path: str):
         if not os.path.exists(file_path):
             raise RuntimeError("Version file not found.")
 
@@ -60,9 +69,6 @@ class PackageUpdater:
             r'__version__\s*=\s*"(\d+)\.(\d+)\.(\d+)"', update_version, content
         )
 
-        with open(file_path, "w") as f:
-            f.write(updated_content)
-
         version_match = re.search(
             r'__version__\s*=\s*"(\d+\.\d+\.\d+)"', updated_content
         )
@@ -71,6 +77,22 @@ class PackageUpdater:
             return version_match.group(1)
         else:
             raise RuntimeError("No version found.")
+
+    def _increment_version(self, file_path: str):
+        if not os.path.exists(file_path):
+            raise RuntimeError("Version file not found.")
+
+        with open(file_path) as f:
+            content = f.read()
+
+        updated_content = re.sub(
+            r'__version__\s*=\s*"(\d+)\.(\d+)\.(\d+)"',
+            f'__version__ = "{self._get_next_version(file_path=file_path)}"',
+            content,
+        )
+
+        with open(file_path, "w") as f:
+            f.write(updated_content)
 
     def _update_changelog(self, file_path: str, version: str):
         if not os.path.exists(file_path):
@@ -95,12 +117,33 @@ class PackageUpdater:
         with open(file_path) as f:
             content = f.read()
 
-        branch_match = re.search(r'main_branch\s*=\s*"(\w+)"', content)
+        branch_match = re.search(r'main_branch\s*=\s*"([\w-]+)"', content)
 
         if branch_match:
             return branch_match.group(1)
         else:
             return "master"
+
+    def get_package_name_from_config(self, file_path: str):
+        with open(file_path) as f:
+            content = f.read()
+
+        module_name_match = re.search(r'module_name\s*=\s*"([\w-]+)"', content)
+        if module_name_match:
+            return module_name_match.group(1)
+
+        package_name_match = re.search(r'package_name\s*=\s*"([\w-]+)"', content)
+
+        if package_name_match:
+            return package_name_match.group(1)
+        else:
+            raise RuntimeError("No package name found.")
+
+    def _check_branch_exists(self, branch_name: str):
+        result = subprocess.run(
+            ["git", "branch", "--list", branch_name], capture_output=True, text=True
+        )
+        return bool(result.stdout.strip())
 
     def process(self):
         for directory in self.PACKAGE_DIR.iterdir():
@@ -108,7 +151,8 @@ class PackageUpdater:
                 directory.is_dir()
                 and (Path(directory) / ".ambient-package-update").is_dir()
             ):
-                self._print_cyan(f"Processing {directory.name}...")
+                self._create_header(package_name=directory.name)
+
                 venv_exec = self.ENVS_DIR / directory.name / "Scripts/python.exe"
                 if not venv_exec.exists():
                     self._print_red("> Venv not found. Aborting.")
@@ -133,6 +177,23 @@ class PackageUpdater:
                 print(f"> Ensure we're on the {main_branch} branch")
                 self._run_command(f"git checkout {main_branch}")
 
+                package_name = self.get_package_name_from_config(
+                    file_path="./.ambient-package-update/metadata.py"
+                )
+                version = self._get_next_version(
+                    file_path=f"./{package_name.replace('-', '_')}/__init__.py"
+                )
+                branch_name = f"maintenance/v{version}"
+                print("> Check if branch already exists")
+                branch_already_exists = False
+                if self._check_branch_exists(branch_name=branch_name):
+                    print("> Switching to git branch")
+                    self._run_command(f"git checkout {branch_name}")
+                    branch_already_exists = True
+                else:
+                    print("> Creating and switching to new git branch")
+                    self._run_command(f"git switch -c {branch_name}")
+
                 print("> Rendering configuration templates")
                 self._run_command(
                     f"{venv_exec} {self._AMBIENT_UPDATER_RENDER_TEMPLATES}"
@@ -141,20 +202,29 @@ class PackageUpdater:
                 print("> Check if something has changed. If not, we're done here")
                 result = subprocess.run(self._GIT_DIFF, capture_output=True, text=True)
                 if result.returncode == 0:
-                    print("> No changes. Skipping package.")
+                    if not branch_already_exists:
+                        print("> Removing newly created local branch")
+                        self._run_command(f"git checkout {main_branch}")
+                        self._run_command(f"git branch -d {branch_name}")
+                    self._print_cyan("> No changes. Skipping package.\n\n")
                     continue
 
                 print("> Incrementing version patch release")
-                version = self._increment_version(
-                    file_path=f"./{directory.name.replace('-', '_')}/__init__.py"
+                self._increment_version(
+                    file_path=f"./{package_name.replace('-', '_')}/__init__.py"
                 )
 
                 print("> Adding release notes to changelog")
                 self._update_changelog(file_path="./CHANGES.md", version=version)
 
                 branch_name = f"maintenance/v{version}"
-                print("> Creating and switching to new git branch")
-                self._run_command(f"git switch -c {branch_name}")
+                print("> Check if branch already exists")
+                if self._check_branch_exists(branch_name=branch_name):
+                    print("> Switching to git branch")
+                    self._run_command(f"git checkout {branch_name}")
+                else:
+                    print("> Creating and switching to new git branch")
+                    self._run_command(f"git switch -c {branch_name}")
 
                 print("> Adding changes to git")
                 self._run_command("git add .")
